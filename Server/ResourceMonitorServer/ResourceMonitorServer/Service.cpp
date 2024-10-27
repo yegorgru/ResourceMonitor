ea9ad000d4853806731e01c7261e749644813a59
yegorgru <yegorgru@gmail.com>
2024-10-27 21:21:35 +0200

    rework Service class on Server side to handle http requests

--- Server/ResourceMonitorServer/ResourceMonitorServer/Service.cpp
+++ Server/ResourceMonitorServer/ResourceMonitorServer/Service.cpp
@@ -1,63 +1,165 @@
 #include "Service.h"
 
 #include <iostream>
+#include <map>
 
 namespace ResourceMonitorServer {
 
 Service::Service(TcpSocketPtr socket)
     : mSocket(socket)
+    , mRequestBuf(4096)
+    , mResponseStatusCode(200)
 {
-
 }
 
 void Service::startHandling() {
     mSelfPtr = shared_from_this();
-    boost::asio::async_read_until(*mSocket.get(), mRequestBuf, '\n',
+    //TODO: check if can use without get()
+    boost::asio::async_read_until(*mSocket.get(), mRequestBuf, "\r\n",
         [this](const boost::system::error_code& ec, std::size_t bytes_transferred)
         {
-            onRequestReceived(ec, bytes_transferred);
+            if (ec.value() != 0) {
+                std::cout << "Error occured! Error code = " << ec.value() << ". Message: " << ec.message();
+
+                if (ec == boost::asio::error::not_found) {
+                    mResponseStatusCode = 413;
+                    sendResponse();
+                    return;
+                }
+                else {
+                    onFinish();
+                    return;
+                }
+            }
+            processRequestLine();
         }
     );
 }
 
-void Service::onRequestReceived(const Error& ec, std::size_t bytesTransferred) {
-    if (ec.value() != 0) {
-        std::cout << "Error occured! Error code = " << ec.value() << ". Message: " << ec.message();
+void Service::processRequestLine()
+{
+    std::string requestLine;
+    std::istream requestStream(&mRequestBuf);
+    std::getline(requestStream, requestLine, '\r');
 
-        onFinish();
+    requestStream.get(); // Remove symbol '\n' from the buffer.
+
+    std::string requestMethod;
+    std::istringstream requestLineStream(requestLine);
+    requestLineStream >> requestMethod;
+
+    if (requestMethod.compare("GET") != 0) {
+        mResponseStatusCode = 501;
+        sendResponse();
         return;
     }
 
-    mResponse = processRequest(mRequestBuf);
+    requestLineStream >> mRequestedResource;
 
-    boost::asio::async_write(*mSocket.get(), boost::asio::buffer(mResponse),
-        [this](
-            const boost::system::error_code& ec,
-            std::size_t bytes_transferred)
-        {
-            onResponseSent(ec,
-            bytes_transferred);
+    std::string request_http_version;
+    requestLineStream >> request_http_version;
+
+    if (request_http_version.compare("HTTP/1.1") != 0) {
+        mResponseStatusCode = 505;
+        sendResponse();
+
+        return;
+    }
+
+    boost::asio::async_read_until(*mSocket.get(),
+        mRequestBuf,
+        "\r\n\r\n",
+        [this] (const boost::system::error_code& ec, std::size_t bytes_transferred) {
+            if (ec.value() != 0) {
+                std::cout << "Error occured! Error code = " << ec.value() << ". Message: " << ec.message();
+
+                if (ec == boost::asio::error::not_found) {
+                    mResponseStatusCode = 413;
+                    sendResponse();
+                    return;
+                }
+                else {
+                    onFinish();
+                    return;
+                }
+            }
+            processHeaders();
         }
     );
 }
 
-void Service::onResponseSent(const Error& ec, std::size_t bytesTransferred) {
-    if (ec.value() != 0) {
-        std::cout << "Error occured! Error code = "
-            << ec.value()
-            << ". Message: " << ec.message();
+void Service::sendResponse() {
+    mSocket->shutdown(boost::asio::ip::tcp::socket::shutdown_receive);
+
+    std::string responseStatusLine = std::string("HTTP/1.1 ") + getStatusLine(mResponseStatusCode) + "\r\n";
+    
+    std::string responseHeadersString;
+    for (const auto& [key, value] : mResponseHeaders) {
+        responseHeadersString += key + ": " + value +"\r\n";
+    }
+    responseHeadersString += "\r\n";
+
+    std::vector<boost::asio::const_buffer> responseBuffers;
+    responseBuffers.push_back(boost::asio::buffer(responseStatusLine));
+    responseBuffers.push_back(boost::asio::buffer(responseHeadersString));
+
+    if (mResponse.length() > 0) {
+        responseBuffers.push_back(boost::asio::buffer(mResponse));
     }
 
-    onFinish();
+    boost::asio::async_write(*mSocket.get(), responseBuffers,
+        [this] (const boost::system::error_code& ec, std::size_t bytes_transferred) {
+            if (ec.value() != 0) {
+                std::cout << "Error occured! Error code = "
+                    << ec.value()
+                    << ". Message: " << ec.message();
+            }
+
+            mSocket->shutdown(boost::asio::ip::tcp::socket::shutdown_both);
+            onFinish();
+        }
+    );
+}
+
+void Service::processHeaders() {
+    std::istream requestStream(&mRequestBuf);
+    std::string headerName, headerValue;
+
+    while (!requestStream.eof()) {
+        std::getline(requestStream, headerName, ':');
+        if (!requestStream.eof()) {
+            std::getline(requestStream, headerValue, '\r');
+            requestStream.get();
+            mRequestHeaders[headerName] = headerValue;
+        }
+    }
+
+    processRequest();
+    sendResponse();
+
+    return;
+}
+
+void Service::processRequest() {
+    mResponse = "Content";
+    mResponseHeaders["content-length"] = std::to_string(mResponse.length());
+}
+
+const std::string& Service::getStatusLine(HttpCode code) {
+    static const std::map<HttpCode, std::string> httpStatusTable =
+    {
+        { 200, "200 OK" },
+        { 404, "404 Not Found" },
+        { 413, "413 Request Entity Too Large" },
+        { 500, "500 Server Error" },
+        { 501, "501 Not Implemented" },
+        { 505, "505 HTTP Version Not Supported" }
+    };
+    return httpStatusTable.at(code);
 }
 
 void Service::onFinish() {
     mSelfPtr.reset();
-}
-
-std::string Service::processRequest(RequestBuf& request) {
-    std::string response = "Response\n";
-    return response;
 }
 
 } // namespace ResourceMonitorServer
--- Server/ResourceMonitorServer/ResourceMonitorServer/Service.h
+++ Server/ResourceMonitorServer/ResourceMonitorServer/Service.h
@@ -2,6 +2,7 @@
 
 #include <memory>
 #include <string>
+#include <map>
 
 #include <boost/asio.hpp>
 
@@ -9,26 +10,35 @@ namespace ResourceMonitorServer {
 
 class Service : public std::enable_shared_from_this<Service> {
 public:
-    using TcpSocketPtr = std::shared_ptr<boost::asio::ip::tcp::socket>;
+    using TcpSocket = boost::asio::ip::tcp::socket;
+    using TcpSocketPtr = std::shared_ptr<TcpSocket>;
+    using HttpCode = unsigned int;
 public:
     Service(TcpSocketPtr socket);
 public:
     void startHandling();
 private:
     using RequestBuf = boost::asio::streambuf;
-    using Error = boost::system::error_code;
+    using ErrorCode = boost::system::error_code;
 private:
-    void onRequestReceived(const Error& ec, std::size_t bytesTransferred);
-    void onResponseSent(const Error& ec, std::size_t bytesTransferred);
+    void processRequestLine();
+    void processHeaders();
+    void processRequest();
+    void sendResponse();
+    const std::string& getStatusLine(HttpCode code);
     void onFinish();
-    std::string processRequest(RequestBuf& request);
 private:
     using ServicePtr = std::shared_ptr<Service>;
+    using HttpHeaders = std::map<std::string, std::string>;
 private:
     ServicePtr mSelfPtr;
     TcpSocketPtr mSocket;
     std::string mResponse;
     RequestBuf mRequestBuf;
+    HttpHeaders mRequestHeaders;
+    std::string mRequestedResource;
+    HttpCode mResponseStatusCode;
+    HttpHeaders mResponseHeaders;
 };
 
 } // namespace ResourceMonitorServer
