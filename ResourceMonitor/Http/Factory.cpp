dc4c063f6cfe34db96ed6af4c3fa0a9cf7edf11e
yegorgru <yegorgru@gmail.com>
2025-04-17 22:08:28 +0300

    simplify cmakelists

--- ResourceMonitor/Http/Factory.cpp
+++ ResourceMonitor/Http/Factory.cpp
@@ -1,43 +1,28 @@
 #include "Factory.h"
 
-#if defined(CLIENT_BOOST_ASIO) + defined(CLIENT_BOOST_BEAST) + defined(CLIENT_POCO) > 1
-#error "Only one client macro can be defined: CLIENT_BOOST_ASIO, CLIENT_BOOST_BEAST, or CLIENT_POCO."
-#endif
-
-#if defined(SERVER_BOOST_ASIO) + defined(SERVER_BOOST_BEAST) + defined(SERVER_POCO) > 1
-#error "Only one server macro can be defined: SERVER_BOOST_ASIO, SERVER_BOOST_BEAST, or SERVER_POCO."
-#endif
-
-#if defined(CLIENT_BOOST_ASIO)
+#if defined(USE_ASIO)
 #include "Asio/Client/Client.h"
-#elif defined(CLIENT_BOOST_BEAST)
-#include "Beast/Client/Client.h"
-#elif defined(CLIENT_POCO)
-#include "Poco/Client/Client.h"
-#else 
-#error "No client macro defined. Define one of CLIENT_BOOST_ASIO, CLIENT_BOOST_BEAST, or CLIENT_POCO."
-#endif
-
-#if defined(SERVER_BOOST_ASIO)
 #include "BoostCommon/Server.h"
 #include "Asio/Server/Service.h"
-#elif defined(SERVER_BOOST_BEAST)
+#elif defined(USE_BEAST)
+#include "Beast/Client/Client.h"
 #include "BoostCommon/Server.h"
 #include "Beast/Server/Service.h"
-#elif defined(SERVER_POCO)
+#elif defined(USE_POCO)
+#include "Poco/Client/Client.h"
 #include "Poco/Server/Server.h"
 #else 
-#error "No server macro defined. Define one of SERVER_BOOST_ASIO, SERVER_BOOST_BEAST, or SERVER_POCO."
+#error "No http implementation selected."
 #endif
 
 namespace Http {
 
 ClientPtr createNetworkClient() {
-#if defined(CLIENT_BOOST_ASIO)
+#if defined(USE_ASIO)
     return std::make_unique<Asio::Client>();
-#elif defined(CLIENT_BOOST_BEAST)
+#elif defined(USE_BEAST)
     return std::make_unique<Beast::Client>();
-#elif defined(CLIENT_POCO)
+#elif defined(USE_POCO)
     return std::make_unique<Poco::Client>();
 #else 
     return nullptr;
@@ -45,13 +30,13 @@ ClientPtr createNetworkClient() {
 }
 
 ServerPtr createNetworkServer() {
-#if defined(SERVER_BOOST_ASIO)
+#if defined(USE_ASIO)
     using ServerType = Boost::Common::Server<boost::asio::io_service, Asio::Service>;
     return std::make_unique<ServerType>();
-#elif defined(SERVER_BOOST_BEAST)
+#elif defined(USE_BEAST)
     using ServerType = Boost::Common::Server<boost::asio::io_context, Beast::Service>;
     return std::make_unique<ServerType>();
-#elif defined(SERVER_POCO)
+#elif defined(USE_POCO)
     return std::make_unique<Poco::Server>();
 #else 
     return nullptr;
