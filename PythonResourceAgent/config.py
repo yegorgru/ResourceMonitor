2f867e08a2b03711fbd2d2daca96ac0eb8eb2922
yegorgru <yegorgru@gmail.com>
2025-05-01 10:22:21 +0300

    fix ip address on resource agent

--- PythonResourceAgent/config.py
+++ PythonResourceAgent/config.py
@@ -1,11 +1,25 @@
-import socket
 import logging
+import socket
 
 LOG_LEVEL = logging.INFO
 LOG_FORMAT = '%(filename)s:%(lineno)d [%(levelname)s] %(message)s'
 
-MACHINE_IP = socket.gethostbyname(socket.gethostname())
 DB_SERVER_URL = "http://localhost:3333"
+
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(0)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.254.254.254', 1))
+        IP = s.getsockname()[0]
+    except Exception:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+
+MACHINE_IP = get_ip()
 
 ENDPOINTS = {
     "basic_info": f"{DB_SERVER_URL}/basic_info/{MACHINE_IP}",
